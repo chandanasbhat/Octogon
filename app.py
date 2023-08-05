9d04847cbdb672fe285efe5ce975e19619a65fec
chandanasbhat <70644789+chandanasbhat@users.noreply.github.com>
2023-08-05 00:22:37 +0000

    added dropdowns

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='Templates')
 
 
 @app.route('/')
 def hello_world():
-  return "hello world"
+  return render_template('home.html')
 
 
 if __name__ == '__main__':
